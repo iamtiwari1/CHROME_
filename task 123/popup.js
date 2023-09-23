a2dc7fc80a44be9de0d4f686a1a3a278ed987354
iamtiwari1 <91432943+iamtiwari1@users.noreply.github.com>
2023-09-23 20:33:32 +0530

    Add files via upload

---
+++ task 123/popup.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById('popupButton');
+    button.addEventListener('click', function() {
+      // Handle button click action here
+      alert("Button clicked!");
+    });
+  });
+  
